df51c2eccad1cea748034a1c717facfc34b138a1
Miracle00 <sergenbag01@gmail.com>
2017-12-19 20:54:55 +0300

    test eklendi

---
+++ YS_ProjectTest/ProjectTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YS_PROJECT;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace YS_ProjectTest
+{
+    [TestClass]
+    public class ProjectTest
+    {
+        [TestMethod]
+        public void STOKDBConnection()
+        {
+            //STOKDB ye bağlantı yapabiliyor mu
+            string baglanti = @"Data Source=TOSHIBAPC\SQLEXPRESS;Initial Catalog=StokDB;Integrated Security=True";
+            SqlConnection baglan = new SqlConnection(baglanti);
+            baglan.Open();
+        }
+
+        [TestMethod]
+        public void SelectFuncCalisiyormu()
+        {
+            //fakülteler tablosunda HFTTF varmı?
+            DB_Operation db = new DB_Operation();
+            List<string[]> ll = db.Select(sqlConnectionString.fakulteIsimleri);            
+            Assert.AreEqual("HFTTF", ll[0][1]);
+        }
+
+        [TestMethod]
+        public void SaveFuncCalisiyormu()
+        {
+            //personellere veri ekleyebiliyormu ?
+            DB_Operation db = new DB_Operation();
+            List<string> test = new List<string>() { "XXX", "YYY" };
+            bool result = db.Save(sqlConnectionString.personelEkle, sqlConnectionString.personelParam, test);
+            Assert.AreEqual(true,result);
+        }
+
+    }
+}
---
+++ YS_ProjectTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YS_ProjectTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YS_ProjectTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9d13ba1-9ee3-406e-8684-d8044ae99da4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
